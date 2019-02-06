a9a63b6c424a8dfc769cc197a39bca73e3f0390b
AsgerPilegaard <asgerpilegaard@gmail.com>
2019-02-06 22:34:45 +0100

    Program Directory is now dynamic.

--- BatchAnalyzer/Program.cs
+++ BatchAnalyzer/Program.cs
@@ -1,21 +1,32 @@
 ﻿using System.Linq;
 using System.IO;
+using System.Reflection;
 using BatchAnalyzer.Services;
 
 namespace BatchAnalyzer
 {
     class Program
     {
-        public const string _dataFolderDirectory = @"C:\Users\asger\OneDrive\Desktop\Scio+\BatchAnalyzer\data";
+        public const string _nameOfApplication = "BatchAnalyzer";
+        public const string _dataFolderDirectory = @"\data";
         public const string _statisticsFileName = "PostSlaughterStatistics.json";
 
         public static void Main(string[] args)
         {
-            var jsonFilesInDirectory = Directory.EnumerateFiles(_dataFolderDirectory).Where(file => file.Remove(0, file.Length - 5) == ".json");
+            var dataPath = GetCurrentDataFolderPath();
+            var jsonFilesInDirectory = Directory.EnumerateFiles(dataPath).Where(file => file.Remove(0, file.Length - 5) == ".json");
             var predictions = FileReaderService.LoadPredictions(jsonFilesInDirectory);
             var slaughterWeights = FileReaderService.LoadSlaughterWeights(jsonFilesInDirectory);
             var slaughterStatistics = StatisticsService.CreateSlaughterStatistics(predictions, slaughterWeights);
-            JsonWriterService.WriteStatisticsFile(slaughterStatistics, _dataFolderDirectory, _statisticsFileName);
+            JsonWriterService.WriteStatisticsFile(slaughterStatistics, dataPath, _statisticsFileName);
+        }
+
+        public static string GetCurrentDataFolderPath()
+        {
+            var executingDirectory = Assembly.GetExecutingAssembly().Location;
+            var baseDirectory = executingDirectory.Remove(executingDirectory.IndexOf(_nameOfApplication));
+            var dataPath = baseDirectory + $@"\{_nameOfApplication}" + _dataFolderDirectory;
+            return dataPath;
         }
     }
 }
